a73d4f7229e6a3c677c57e3e827fa8865e011b02
Mike Lee Williams <mike@mike.place>
2024-01-06 00:48:29 -0500

    Use modal

--- neuripsjson.py
+++ neuripsjson.py
@@ -1,20 +1,22 @@
-import asyncio
-import datetime as dt
 import json
-import sys
 
 import bs4
-import httpx
-import tqdm
-import tqdm.asyncio
+import modal
+import requests
 
 urlroot = "https://papers.nips.cc"
 
-limits = httpx.Limits(max_keepalive_connections=4, max_connections=8)
+stub = modal.Stub(
+    image=modal.Image.debian_slim().pip_install(
+        "beautifulsoup4",
+        "requests",
+    ),
+)
 
 
-async def add_abstract_to_paper_with_client(paper, client):
-    text = (await client.get(paper["url"])).text
+@stub.function()
+def add_abstract_to_paper(paper):
+    text = requests.get(paper["url"]).text
     page = bs4.BeautifulSoup(text, "html.parser")
     try:
         ps = page.find("h4", text="Abstract").find_next_siblings("p")
@@ -33,7 +35,7 @@ def get_paper_from_li(bullet):
 
 
 def get_papers_from_url(url):
-    r = httpx.get(url)
+    r = requests.get(url)
     r.raise_for_status()
     page = bs4.BeautifulSoup(r.text, "html.parser")
     papers = [
@@ -43,56 +45,22 @@ def get_papers_from_url(url):
     return papers
 
 
-async def get_year(year):
+@stub.function()
+def get_papers_for_year(year):
     papers = get_papers_from_url(f"{urlroot}/paper/{str(year)}")
     for paper in papers:
         paper["year"] = year
-    async with httpx.AsyncClient(timeout=None, limits=limits) as client:
-        tasks = [add_abstract_to_paper_with_client(paper, client) for paper in papers]
-        papers = await tqdm.asyncio.tqdm.gather(*tasks, desc=str(year))
     return papers
 
 
-async def get_all_years(last_year=None):
-    if last_year is None:
-        last_year = dt.datetime.now().year - 1
-    years = []
-    for year in range(1987, last_year + 1):
-        years.append(await get_year(year))
-    return sum(years, [])
+def get_all_years():
+    papers = sum(get_papers_for_year.map(range(1987, 2023)), [])
+    papers = list(add_abstract_to_paper.map(papers))
+    return papers
 
 
-async def load_and_append_year(year):
-    with open("neurips.json") as infile:
-        old_papers = json.load(infile)
-    print("Loaded {} papers from neurips.json".format(len(old_papers)))
-    new_papers = await get_year(year)
-    print("Added {} papers from {}".format(len(new_papers), year))
-    return old_papers + new_papers
-
-
-def print_usage_and_exit():
-    usage = "command must be one of 'create' or 'add <year>'"
-    print(usage)
-    raise SystemExit(1)
-
-
-if __name__ == "__main__":
-    try:
-        command = sys.argv[1]
-    except IndexError:
-        print_usage_and_exit()
-
-    if command == "add":
-        try:
-            year = sys.argv[2]
-        except (IndexError, ValueError):
-            print_usage_and_exit()
-        papers = asyncio.run(load_and_append_year(year))
-    elif command == "create":
-        papers = asyncio.run(get_all_years())
-    else:
-        print_usage_and_exit()
-
+@stub.local_entrypoint()
+def main():
+    papers = get_all_years()
     with open("neurips.json", "w") as fp:
         json.dump(papers, fp, indent=2)
